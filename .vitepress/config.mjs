cc3e143aa2955111170ad873cdd8ab9b8ea6d7e6
Kobe.Lu <kobeluhaonan@gmail.com>
2025-02-11 14:38:47 +0800

    update

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -11,7 +11,7 @@ const vitepressConfig = {
   lastUpdated: false,
   lang: 'zh-CN',
   head: [
-      ['link', { rel: 'icon', href: '/pages/favicon.ico' }],
+      ['link', { rel: 'icon', href: '/favicon.ico' }],
       ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
       ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com' }],
       ['link', { href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&family=ZCOOL+KuaiLe&display=swap', rel: 'stylesheet' }],
